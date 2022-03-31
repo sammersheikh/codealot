b1c22932d70446c1cea08181489d27481471040b
sammersheikh <sammer.sheikh@outlook.com>
2022-03-31 14:14:59 -0700

    add delete comment functionality

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,8 +1,8 @@
-const methodOverride = require('method-override')
 const Post = require('../models/post')
 
 module.exports = {
     addComment,
+    delete: deleteComment,
 }
 
 function addComment(req, res) {
@@ -16,5 +16,22 @@ function addComment(req, res) {
         post.save(function(err) {
             res.redirect(`/posts/${post._id}`)
         })
+    })
+}
+
+function deleteComment(req, res, next) {
+    Post.findOne({ 'comments._id':req.params.id })
+    .then(function(post) {
+        console.log('hello')
+        const comment = post.comments.id(req.params.id)
+        if(!comment.user.equals(req.user._id)) return res.redirect(`/posts/${post._id}`)
+        comment.remove()
+        post.save()
+        .then(function() {
+            res.redirect(`/posts/${post._id}`)
+        }) .catch(function(err) {
+            return next(err)
+        })
+
     })
 }
--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const commentsCtrl = require('../controllers/comments')
 
 router.post('/posts/:id/comments', commentsCtrl.addComment)
+router.delete('/comments/:id', commentsCtrl.delete)
 
 
 module.exports = router;
